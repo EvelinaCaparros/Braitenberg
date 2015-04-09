556542ab58de297494566f8fa669659801bcb27d
Kevin <kevin@localhost.localdomain>
2015-04-09 12:29:57 -0500

    Cleaned up main

--- main.py
+++ main.py
@@ -1,26 +1,10 @@
 #!/usr/bin/env python
 
-from World import *
-from Light import *
-from Bot import *
 from Window import *
-
 
 def main():
     w = Window()
     w.display()
-    world = World()
-    l = Light()
-    l.move(2,2)
-    world.addLight(l)
-    b = Bot(4,4)
-    print b.getLocation()
-    b.process(world)
-    print b.getLocation()
-    print l.getLocation()
-    print l.getLocation()
-    print 'hi'
-
 
 if __name__ == '__main__':
     main()
