36be0547423b672750a4bc5d6b38aa74c0e4f4ec
Kevin <kevin@localhost.localdomain>
2015-04-09 10:21:52 -0500

    Made bots rotatable and fixed the images accordingly

--- Bot.py
+++ Bot.py
@@ -10,7 +10,7 @@ class Bot:
     
     _stepDistance = 1
 
-    def __init__(self, master=0, x=0, y=0, angle=0, k11=1, k12=0, k21=0, k22=0):
+    def __init__(self, master=0, x=0, y=0, angle=0, sizeX=100, sizeY=100, k11=1, k12=0, k21=0, k22=0):
         self._x = int(x)
         self._y = int(y)
         self._angle = int(angle)
@@ -19,11 +19,17 @@ class Bot:
         self._k21 = int(k21)
         self._k22 = int(k22)
         self._master = master
-        image = Image.open('spider.png')
-        imagetk = ImageTk.PhotoImage(image)
+        self._sizeX = sizeX
+        self._sizeY = sizeY
+        self._filename='spider.png'
+        image = Image.open(self._filename)
+        image = image.resize((self._sizeX,self._sizeY), Image.ANTIALIAS)
+        imagetk = ImageTk.PhotoImage(image.rotate(self._angle))
         label = Label(master, image=imagetk)
         label.image = imagetk
         label.place(x=x,y=y)
+        self._container = label
+
 
     def setMatrix(self, k11=1, k12=0, k21=0, k22=0):
         self._k11 = k11
@@ -32,8 +38,13 @@ class Bot:
         self._k22 = k22
 
     def _update(self):
-        pass
-        self._graphic.place(x=self._x, y=self._y)
+        image = Image.open(self._filename)
+        image = image.resize((self._sizeX,self._sizeY), Image.ANTIALIAS)
+        imagetk = ImageTk.PhotoImage(image.rotate(self._angle))
+        self._container.destroy()
+        self._container = Label(self._master, image=imagetk)
+        self._container.image = imagetk
+        self._container.place(x=self._x, y=self._y)
 
     def process(self, world):
         # TODO: make these actual sensor locations
@@ -50,8 +61,8 @@ class Bot:
             rightTotal += light.getStrength(sensorxR, sensoryR)
 
         # TODO: move this according to the k matrix and _stepDistance
-        self._x += leftTotal
-        self._y += rightTotal
+        self._x += leftTotal + 10
+        self._y += rightTotal + 10
 
         self._update()
 
